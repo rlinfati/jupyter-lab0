da482dfc1301eda0384c509c747621dc3a1bde3c
Rodrigo Linfati <rodrigo@linfati.cl>
2025-01-27 20:18:22 -0300

    Anaconda and CUDA

--- lab0-refresh.sh
+++ lab0-refresh.sh
@@ -3,6 +3,9 @@
 set -x
 
 sudo podman pull registry.access.redhat.com/ubi9/ubi:latest
+sudo podman pull registry.access.redhat.com/ubi9/ubi-init:latest
+sudo podman pull registry.access.redhat.com/ubi9/ubi-minimal:latest
+sudo podman pull registry.access.redhat.com/ubi9/ubi-micro:latest
 sudo podman pull docker.io/library/caddy:latest
 
 sudo podman pull quay.io/jupyter/julia-notebook:latest
@@ -15,19 +18,22 @@ sudo podman image prune --force
 
 if [[ $(uname -m) == "x86_64" ]]; then
     sudo podman pull docker.io/rlinfati/jupyter-lab0:hub-999
-
     sudo podman pull docker.io/rlinfati/jupyter-lab0:julia-110
-    sudo podman pull docker.io/rlinfati/jupyter-lab0:cudalab-110
-
     sudo podman pull docker.io/rlinfati/jupyter-lab0:julia-111
-    sudo podman pull docker.io/rlinfati/jupyter-lab0:cudalab-111
-
+    sudo podman pull docker.io/rlinfati/jupyter-lab0:Anaconda-999
+    if [[ -c /dev/nvidiactl ]]; then
+        sudo podman pull docker.io/rlinfati/jupyter-lab0:juliacuda-111
+        sudo podman pull docker.io/rlinfati/jupyter-lab0:NVpytorch-999
+        sudo podman pull docker.io/rlinfati/jupyter-lab0:NVtensorflow-999
+    fi
     sudo podman image prune --force
 else
-    sudo podman build --tag rlinfati/jupyter-lab0:hub-999       github.com/rlinfati/jupyter-lab0 --file Dockerfile.HUB && \
-    sudo podman build                                           github.com/rlinfati/jupyter-lab0 --file Dockerfile.ORlib --build-arg UNAMEM1=aarch64 --build-arg CPU_TARGET="neoverse-n1" --secret id=GUROBIWLS && \
-    sudo podman build --tag rlinfati/jupyter-lab0:julia-999     github.com/rlinfati/jupyter-lab0 --file Dockerfile.JULIA --build-arg UNAMEM1=aarch64 --build-arg CPU_TARGET="neoverse-n1" --secret id=GUROBIWLS && \
-    sudo podman build --tag rlinfati/jupyter-lab0:cudalab-999   github.com/rlinfati/jupyter-lab0 --file Dockerfile.CUDAlab && \
+    sudo podman pull docker.io/rlinfati/jupyter-lab0:hub-999
+    sudo podman pull docker.io/rlinfati/jupyter-lab0:julia-999
+    sudo podman pull docker.io/rlinfati/jupyter-lab0:Anaconda-999
+
+    #sudo podman build --tag rlinfati/jupyter-lab0:julia-999 github.com/rlinfati/jupyter-lab0 --file Dockerfile.JULIA --build-arg CPU_TARGET="generic;cortex-a76;neoverse-n1" --secret id=GUROBIWLS
+    sudo systemctl restart jupyter-hub.service
     sudo podman image prune --force
 fi
 
