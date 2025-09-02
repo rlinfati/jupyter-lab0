32b5a410e6a2772aec62ba72247b5036d7631ef0
Rodrigo Linfati <rodrigo@linfati.cl>
2025-09-01 23:57:35 -0400

    Kubernetes

--- lab0-refresh.sh
+++ lab0-refresh.sh
@@ -2,17 +2,7 @@
 
 set -ex
 
-sudo podman pull registry.access.redhat.com/ubi9/ubi:latest
-sudo podman pull registry.access.redhat.com/ubi9/ubi-init:latest
-sudo podman pull registry.access.redhat.com/ubi9/ubi-minimal:latest
-sudo podman pull registry.access.redhat.com/ubi9/ubi-micro:latest
-
-sudo podman pull registry.access.redhat.com/ubi10/ubi:latest
-sudo podman pull registry.access.redhat.com/ubi10/ubi-init:latest
-sudo podman pull registry.access.redhat.com/ubi10/ubi-minimal:latest
-sudo podman pull registry.access.redhat.com/ubi10/ubi-micro:latest
-
-sudo podman pull docker.io/library/caddy:latest
+sudo podman pull docker.io/library/caddy:alpine
 
 sudo podman pull quay.io/jupyter/julia-notebook:latest
 sudo podman pull quay.io/jupyter/scipy-notebook:latest
@@ -23,7 +13,6 @@ sudo podman pull quay.io/jupyter/pytorch-notebook:latest
 sudo podman image prune --force
 
 sudo podman pull docker.io/rlinfati/jupyter-lab0:hub
-sudo podman pull docker.io/rlinfati/jupyter-lab0:k8s
 sudo podman pull docker.io/rlinfati/jupyter-lab0:Anaconda
 sudo podman pull docker.io/rlinfati/jupyter-lab0:julia-110
 sudo podman pull docker.io/rlinfati/jupyter-lab0:julia-111
@@ -36,11 +25,10 @@ if [[ -c /dev/nvidiactl ]]; then
     sudo podman pull quay.io/jupyter/pytorch-notebook:cuda12-latest
 fi
 
-sudo systemctl restart jupyter-hub.service
 sudo podman image prune --force
-
 sudo podman image ls -a
 sudo podman container ls
+
 sudo systemctl start podman-auto-update
 sudo podman image ls -a
 sudo podman container ls
